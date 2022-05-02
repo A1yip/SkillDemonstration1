5a2e8dc7cedfbe34942862b9b09c4f20f7e1000a
A1yip <a1yip@ucsd.edu>
2022-05-01 20:46:52 -0700

    integer files

--- Integer.java
+++ Integer.java
@@ -1,1 +1,9 @@
+public class Integer {
+    int integer;
+    public Integer(int i) {
+        this.integer =i;
+    }
+    public void addition(int i) {
 
+    }
+}
---
+++ IntegerTester.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+public class IntegerTester {
+    @Test
+    public void additionTester() {
+        Integer test1 = new Integer(3);
+        test1.addition(4);
+        assertEquals(7, test1);
+    }
+}
